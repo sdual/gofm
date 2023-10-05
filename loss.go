b4f5ce0c6d5f27a7e9bd2107de48d3a852f8434f
sdual <shr.nrht.0623@gmail.com>
2023-10-05 19:02:28 +0900

    add differential of cross entropy

--- loss.go
+++ loss.go
@@ -15,13 +15,52 @@ func BinaryCrossEntropy(targets []float64, preds []float64) float64 {
 		return 0.0
 	}
 
-	value := 0.0
+	totalValue := 0.0
 	for i, target := range targets {
-		value += -target*math.Log(preds[i]+epsilon) - (1.0-target)*math.Log(1.0-preds[i]+epsilon)
+		validateProbability(target)
+		pred := preds[i]
+		validateProbability(pred)
+
+		totalValue += -target*math.Log(pred+epsilon) - (1.0-target)*math.Log(1.0-pred+epsilon)
 	}
-	return value / float64(len(targets))
+	return totalValue / float64(len(targets))
 }
 
-func DiffBinaryCrossEntropy() float64 {
-	return 0.0
+func DiffBinaryCrossEntropy(targets []float64, preds []float64) float64 {
+	if len(targets) != len(preds) {
+		panic(fmt.Sprintf("targets must have the same length of preds. targets: %d, preds: %d", len(targets), len(preds)))
+	}
+	if len(targets) == 0 {
+		return 0.0
+	}
+
+	totalValue := 0.0
+	for i, target := range targets {
+		totalValue += diffBinaryCrossEntropyElement(target, preds[i])
+	}
+
+	return totalValue / float64(len(targets))
+}
+
+func diffBinaryCrossEntropyElement(target float64, pred float64) float64 {
+	validateProbability(target)
+	validateProbability(pred)
+
+	if pred == 1.0 {
+		pred = 1.0 - epsilon
+	}
+	if pred == 0.0 {
+		pred = 0.0 + epsilon
+	}
+	return target/pred - (1.0-target)/(1.0-pred)
+}
+
+func Sigmoid(x float64) float64 {
+	return 1.0 / (1.0 + math.Exp(-x))
+}
+
+func validateProbability(p float64) {
+	if p < 0.0 || p > 1.0 {
+		panic("probability must be [0.0, 1.0]")
+	}
 }
--- loss_test.go
+++ loss_test.go
@@ -37,3 +37,39 @@ func TestTotalBinaryCrossEntropy(t *testing.T) {
 		})
 	}
 }
+
+func TestDiffBinaryCrossEntropy(t *testing.T) {
+	type args struct {
+		targets []float64
+		preds   []float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{
+			name: "DiffBinaryCrossEntropy function calculate differential of cross entropy",
+			args: args{
+				targets: []float64{1.0, 0.0, 1.0},
+				preds:   []float64{0.4, 0.5, 0.9},
+			},
+			want: 0.5370370370370371,
+		},
+		{
+			name: "if DiffBinaryCrossEntropy function takes empty slices, 0.0 is returned",
+			args: args{
+				targets: []float64{},
+				preds:   []float64{},
+			},
+			want: 0.0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := DiffBinaryCrossEntropy(tt.args.targets, tt.args.preds); got != tt.want {
+				t.Errorf("DiffBinaryCrossEntropy() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
